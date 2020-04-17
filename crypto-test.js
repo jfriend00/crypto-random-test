662303b8bd7749aed392c9f6ab10f2e2d3d82fe8
jfriend00 <jfriend00@users.noreply.github.com>
2020-04-17 13:38:43 -0700

    Update crypto-test.js
    Improve logging, code cleanup

--- crypto-test.js
+++ crypto-test.js
@@ -5,6 +5,9 @@ const fsp = fs.promises;
 const path = require('path');
 const {fastReadFileLines} = require('./fast-read-file.js');
 
+// create base58 encoder that uses only alpha numerics and leaves out O and 0 which get confused
+const base58Encode = require('base-x')('123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz').encode;
+
 function delay(t, v) {
     return new Promise(resolve => {
         setTimeout(resolve, t, v);
@@ -17,7 +20,7 @@ function addCommas(str) {
         len = main.length,
         output = "",
         i = len - 1;
-    
+
     while(i >= 0) {
         output = main.charAt(i) + output;
         if ((len - i) % 3 === 0 && i > 0) {
@@ -32,24 +35,88 @@ function addCommas(str) {
     return output;
 }
 
-// make a unique filename using first several letters of
+// logging function to output progress every so often based on timeout
+// so you see regular progress, but don't overwhelm the console
+// You can call getLogger() multiple times to get separate loggers with
+// separate timing and triggers
+function getLogger(delay = 1000, skipInitial = true) {
+    // args here are passed directly to console.log
+    let lastOutputTime = 0;
+    let pendingOutput;               // array of arguments for console.log()
+    let pendingTimer;                // timer for next output
+
+    const fn = function(...args) {
+        if (skipInitial && lastOutputTime === 0) {
+            lastOutputTime = Date.now();
+        }
+        // if we haven't sent anything in awhile, just send this one now
+        let now = Date.now();
+        if (now - lastOutputTime > delay) {
+            console.log(...args);
+            lastOutputTime = now;
+        } else {
+            // we did send some logging recently, so queue this
+            // this will overwrite any previous output
+            // if there's a timer, keep it running
+            // if no timer, set a new one, based on lastOutputTime
+            pendingOutput = args;
+            if (!pendingTimer) {
+                pendingTimer = setTimeout(() => {
+                    // delay time has past since any output was sent
+                    fn.flush();
+                }, now - lastOutputTime + delay);
+            }
+        }
+
+    }
+
+    // clear any pending output
+    fn.flush = function() {
+        if (pendingTimer) {
+            clearTimeout(pendingTimer);
+            pendingTimer = null;
+        }
+        if (pendingOutput) {
+
+            console.log(...pendingOutput);
+            pendingOutput = null;
+        }
+        lastOutputTime = Date.now();
+    }
+
+    // flush, then output this log msg right now
+    fn.now = function(...args) {
+        fn.flush();
+        console.log(...args);
+    }
+
+    return fn;
+}
+
+const log = getLogger();
+
+
+// make a unique filename using first few letters of
 // the string.  Strings are case sensitive, bucket filenames
 // cannot be so it has to be case neutralized while retaining
 // uniqueness
 function makeBucketKey(str) {
-    let piece = str.substr(0,2);
+    // with base58 algorithm, the first character does not have as much randomness
+    // so we pick the 2nd and 3rd characters for the bucket keys and this makes
+    // much more even buckets
+    let piece = str.substr(1,2);
     let filename = [];
     // double up each character, but
     for (let ch of piece) {
         filename.push(ch);
         if (ch >= 'a' && ch <= 'z') {
-            filename.push("_")
+            filename.push("_");
         } else {
             filename.push(ch);
         }
     }
     return filename.join("").toLowerCase();
-} 
+}
 
 // this value times the number of total buckets has to fit in memory
 const bucketCacheMax = 3000;
@@ -60,7 +127,7 @@ class Bucket {
         this.filename = filename;
         this.cnt = 0;
         this.writeToDisk = writeToDisk;
-        
+
         // We dither the bucketCacheMax so that buckets aren't all trying to write at the same time
         // After they write once (and are thus spread out in time), then they will reset to full cache size
         let dither = Math.floor(Math.random() * bucketCacheMax) + 10;
@@ -89,44 +156,48 @@ class Bucket {
             if (this.flushPending) {
                 throw new Error("Can't call flush() when flush is already in progress");
             }
-            
+
             function flushNow() {
                 this.flushPending = true;
                 return fsp.appendFile(this.filename, data).finally(() => {
                     this.flushPending = false;
                 });
             }
-            
+
             // we write to disk with retry because we once go EBUSY (perhaps from a backup program)
-            
+
             let retryCntr = 0;
             const retryMax = 10;
             const retryDelay = 200;
             const retryBackoff = 200;
             let lastErr;
-            
+
             function flushRetry() {
                 if (retryCntr > retryMax) {
                     throw lastErr;
                 }
                 return flushNow.call(this).catch(err => {
+                    // On a few runs, we got EBUSY errors when flushing
+                    // My guess is that this was a backup program contending for access
+                    // So, I implemented a backoff retry algorithm to make sure we don't lose
+                    // data in the middle of a multi-hour run
                     lastErr = err;
-                    console.log("flushNow error, retrying...", err);
+                    log.now("flushNow error, retrying...", err);
                     return delay(retryDelay + (retryCntr++ * retryBackoff)).then(() => {
                         return flushRetry.call(this);
                     });
                 });
             }
-            
+
             return flushRetry.call(this);
         }
         this.items.length = 0;
     }
-    
+
     delete() {
         return fsp.unlink(this.filename);
     }
-    
+
     get size() {
         return this.cnt;
     }
@@ -167,21 +238,22 @@ class BucketCollection {
         for (let bucket of this.buckets.values()) {
             max = Math.max(max, bucket.size);
         }
-        return max;        
+        return max;
     }
-    
+
 }
 
 // program options
-let numToTry = 100_000;
+let numToTry = 100_000;           // run this many iterations if no number passed on command line
 let writeToDisk = true;
 let cleanupBucketFiles = true;
 let skipAnalyze = false;
 let analyzeOnly = false;
 
-// -nodisk        don't write to disk
-// -nocleanup     erase bucket files when done
-// -analyzeonly   analyze files in bucket directory only
+// -noDisk        don't write to disk
+// -noCleanup     erase bucket files when done
+// -analyzeOnly   analyze files in bucket directory only
+// -skipAnalyze   skip the analysis, just generate the bucket files
 if (process.argv.length > 2) {
     let args = process.argv.slice(2);
     for (let arg of args) {
@@ -214,121 +286,120 @@ let bucketDir = path.join(__dirname, "buckets");
 
 let collection = new BucketCollection(bucketDir, writeToDisk);
 
-console.log(`Running ${addCommas(numToTry)} random ids`);
-
-const debugMultiple = 100_000;
+log.now(`Running ${addCommas(numToTry)} random ids`);
 
 async function analyze() {
-    let cntr = 0;
-    const cntrProgress = 10;
-    const cntrProgressN = 10n;
-    let buffer = null;
-    let times = [];
-    
-    async function processFile(file) {
-        if (cntr !== 0 && cntr % cntrProgress === 0) {
-            let sum = 0n;
-            for (let i = 0; i < cntrProgress; i++) {
-                sum += times[i];
-            }
-            console.log(`Checking bucket #${cntr}, Average readFileTime = ${sum / cntrProgressN}`);
-            times.length = 0;
-        }
-        ++cntr;
-        
-        let set = new Set();
-        
-        let startT = process.hrtime.bigint();
+    try {
+        let cntr = 0;
+        const cntrProgress = 10;
+        const cntrProgressN = 10n;
         let buffer = null;
-        let result = await fastReadFileLines(file, buffer);
-        let data = result.lines;
-        
-        // keep reusing buffer which may have been made larger since last time
-        buffer = result.buffer;
-        
-        //let data = (await fsp.readFile(file, "utf8")).split("\n");
-        let afterReadFileT = process.hrtime.bigint();
-        for (const lineData of data) {
-            let line = lineData.trim();
-            if (line) {
-                if (set.has(line)) {
-                    console.log(`Found conflict on ${data}`);
-                } else {
-                    set.add(line);
+        let times = [];
+
+        async function processFile(file) {
+            if (cntr !== 0 && cntr % cntrProgress === 0) {
+                let sum = 0n;
+                for (let i = 0; i < cntrProgress; i++) {
+                    sum += times[i];
+                }
+                // log(`Checking bucket #${cntr}, Average readFileTime = ${sum / cntrProgressN}`);
+                // log(`Checking bucket #${cntr}`);
+                log(`Checking bucket #${cntr}`);
+                times.length = 0;
+            }
+            ++cntr;
+
+            let set = new Set();
+
+            let startT = process.hrtime.bigint();
+            let buffer = null;
+            let result = await fastReadFileLines(file, buffer);
+            let data = result.lines;
+
+            // keep reusing buffer which may have been made larger since last time
+            buffer = result.buffer;
+
+            //let data = (await fsp.readFile(file, "utf8")).split("\n");
+            let afterReadFileT = process.hrtime.bigint();
+            for (const lineData of data) {
+                let line = lineData.trim();
+                if (line) {
+                    if (set.has(line)) {
+                        log.flush();
+                        log.now(`Found conflict on ${data}`);
+                        log.now(`Exiting program, bucket files preserved.`)
+                        process.exit(1);
+                    } else {
+                        set.add(line);
+                    }
                 }
             }
+            let loopT = process.hrtime.bigint();
+            let divisor = 1000n;
+            let readFileTime = (afterReadFileT - startT) / divisor;
+            times.push(readFileTime);
         }
-        let loopT = process.hrtime.bigint();
-        let divisor = 1000n;
-        let readFileTime = (afterReadFileT - startT) / divisor;
-        times.push(readFileTime);
-        // console.log(`readFileTime = ${readFileTime}, loopTime = ${(loopT - afterReadFileT) / divisor}`);
-        
-        /*
-        
-        let rl = readline.createInterface({input:fs.createReadStream(file), crlfDelay: Infinity});
-        for await (const line of rl) {
-            let data = line.trim();
-            if (data) {
-                if (set.has(data)) {
-                    console.log(`Found conflict on ${data}`);
-                } else {
-                    set.add(data);
-                }
+
+        if (analyzeOnly) {
+            let files = await fsp.readdir(bucketDir);
+            for (let file of files) {
+                let fullPath = path.join(bucketDir, file)
+                await processFile(fullPath);
+            }
+        } else {
+            for (let bucket of collection.buckets.values()) {
+                await processFile(bucket.filename);
             }
         }
-
-        */
-
-
-    }
-        
-    if (analyzeOnly) {
-        let files = await fsp.readdir(bucketDir);
-        for (let file of files) {
-            let fullPath = path.join(bucketDir, file)
-            await processFile(fullPath);
-        }
-    } else {
-        for (let bucket of collection.buckets.values()) {
-            await processFile(bucket.filename);
-        }
+    } finally {
+        log.flush();
     }
 }
 
 async function makeRandoms() {
-    let start = Date.now();
-    
-    if (analyzeOnly) {
-        return analyze();
-    }
-    
-    for (let i = 0; i < numToTry; i++) {
-        if (i !== 0 && i % debugMultiple === 0) {
-            console.log(`Attempt #${addCommas(i)}`);
-        }
-        const idSeed = crypto.randomBytes(16).toString('base64');
-//        const idSeed = crypto.randomBytes(16).toString('base64') + '' + Date.now();
-        const orderId = idSeed.toString('base64').replace(/[\/\+\=]/g, '');
-        //console.log(orderId);
+    try {
+        let start = Date.now();
 
-        let bucketKey = makeBucketKey(orderId);
-        await collection.add(bucketKey, orderId);
-    }
-    console.log(`Total buckets: ${collection.size}, Max bucket size: ${collection.getMaxBucketSize()}`);
-    //console.log(`No dups found after ${addCommas(numToTry)} attempts`);
-    await collection.flush();
-    
-    let delta = Date.now() - start;
-    console.log(`Run time for creating buckets: ${addCommas(delta)}ms, ${addCommas((delta / numToTry) * 1000)}ms per thousand`);
-    
-    if (!skipAnalyze) {
-        console.log("Analyzing buckets...")
-        await analyze();
-    }
-    if (cleanupBucketFiles) {
-        console.log("Cleaning up buckets...")
-        await collection.delete();
+        if (analyzeOnly) {
+            return analyze();
+        }
+
+        // how often to out
+        const progressMultiple = 100_000;
+
+        for (let i = 0; i < numToTry; i++) {
+            if (i !== 0 && i % progressMultiple === 0) {
+                log(`Generating #${addCommas(i)}`);
+            }
+            // original author's code (which lost random characters)
+            // const string = crypto.randomBytes(16).toString('base64') + '' + Date.now();
+            // const orderId = Buffer.from(idSeed).toString('base64').replace(/[\/\+\=]/g, '');
+
+            // my first edit to keep all random characters by replacing with chars allowed in a filename
+            // const idSeed = crypto.randomBytes(16).toString('base64') + '' + Date.now();
+            // const orderId = idSeed.toString('base64').replace(/=/g, '').replace(/\+/g, "-").replace(/\//g, "~");
+
+            // new base58 encoding algorithm, also drops the Date.now() as it just isn't needed
+            const orderId = base58Encode(crypto.randomBytes(16));
+            const bucketKey = makeBucketKey(orderId);
+            await collection.add(bucketKey, orderId);
+        }
+        log.now(`Total buckets: ${collection.size}, Max bucket size: ${collection.getMaxBucketSize()}`);
+        await collection.flush();
+
+        let delta = Date.now() - start;
+        log.now(`Run time for creating buckets: ${addCommas(delta)}ms, ${addCommas((delta / numToTry) * 1000)}ms per thousand`);
+
+        if (!skipAnalyze) {
+            log.now("Analyzing buckets...")
+            await analyze();
+        }
+        if (cleanupBucketFiles) {
+            log.now("Cleaning up buckets...");
+            await collection.delete();
+        }
+    } finally {
+        log.flush();
     }
 }
 
