9cbee161d95bdc658dec2fbe1ca69c41d3136319
jfriend00 <jfriend00@users.noreply.github.com>
2020-04-22 23:17:37 -0700

    Move Bench to module

--- read-speed.js
+++ read-speed.js
@@ -8,6 +8,7 @@ const { addCommas } = require('../str-utils');
 const { getLogger } = require('../delay-logger');
 const { mapConcurrent, Deferred } = require('../async-utils');
 const { Worker } = require('worker_threads');
+const { Bench } = require('../measure');
 
 const log = getLogger();
 
@@ -30,33 +31,6 @@ let {
     preOpenFiles,
     dir: sourceDir,
 } = processArgs(spec);
-
-class Bench {
-    constructor() {
-        this.cumT = 0n;
-    }
-    markBegin() {
-        this.startT = process.hrtime.bigint();
-    }
-    markEnd() {
-        this.cumT += process.hrtime.bigint() - this.startT;
-    }
-    get ns() {
-        return this.cumT;
-    }
-    get ms() {
-        return Number(this.cumT) / (1000 * 1000);
-    }
-    get sec() {
-        return Number(this.cumT) / (1000 * 1000 * 1000);
-    }
-    formatMs(decimals = 20) {
-        return `${addCommas(this.ms.toFixed(decimals))} ms`;
-    }
-    formatSec(decimals = 20) {
-        return `${addCommas(this.sec.toFixed(decimals))} sec`;
-    }
-}
 
 class WorkerList {
     constructor() {
